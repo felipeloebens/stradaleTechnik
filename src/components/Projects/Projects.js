09f3f71a6e0503f8425dde1b7de09e8f07bf9648
Felipe <Felipe@Felipes-MacBook-Pro.local>
2021-11-21 14:31:37 -0300

    teste instagram embeded

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import InstagramEmbed from 'react-instagram-embed';
 
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
@@ -13,6 +14,19 @@ import bitsOfCode from "../../Assets/Projects/blog.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
+      <InstagramEmbed
+          url='https://instagr.am/p/Zw9o4/'
+          clientAccessToken='123|456'
+          maxWidth={320}
+          hideCaption={false}
+          containerTagName='div'
+          protocol=''
+          injectScript
+          onLoading={() => {}}
+          onSuccess={() => {}}
+          onAfterRender={() => {}}
+          onFailure={() => {}}
+        />
       <Particle />
       <Container>
         <h1 className="project-heading">
