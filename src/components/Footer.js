4a404987b857b98f6f9a7e3713b642f95b96fe89
FelipeL <felipe@fockink.ind.br>
2021-11-23 14:27:28 -0300

    correcao links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://www.instagram.com/stradale.technik/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
